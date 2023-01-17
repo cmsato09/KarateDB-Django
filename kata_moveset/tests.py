ac946e1e6b2847c0b885573c49b277008d5a4e32
cmsato09 <chrismsato@gmail.com>
2023-01-17 01:22:55 -0800

    test: unittest for home_page
    - url mapped empty homepage

--- kata_moveset/tests.py
+++ kata_moveset/tests.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
+from django.urls import resolve
+from kata_moveset.views import home_page
 
 
-class DelibrateFailTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 5)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
+# class DelibrateFailTest(TestCase):
+#
+#     def test_bad_math(self):
+#         self.assertEqual(1 + 1, 5)
