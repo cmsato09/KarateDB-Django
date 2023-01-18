c24d79e5498497feb2223149cdabcf4f04b05b6b
cmsato09 <chrismsato@gmail.com>
2023-01-18 14:52:55 -0800

    migration based on altered model of previous commit

--- kata_moveset/admin.py
+++ kata_moveset/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Kata, Stances, Move, Technique
+from .models import Kata, Stance, Move, Technique
 
 admin.site.register(Kata)
-admin.site.register(Stances)
+admin.site.register(Stance)
 admin.site.register(Move)
 admin.site.register(Technique)
