308bd46b6e3ed12f0ca2bc373fd5845fd7f9f7e8
cmsato09 <chrismsato@gmail.com>
2023-01-23 14:35:30 -0800

    adminsite: added ModelAdmin for Kata, Stance, Technique

--- kata_moveset/admin.py
+++ kata_moveset/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from .models import Kata, Stance, Move, Technique
+from .models import Kata, Stance, Move, Technique, TechniqueToMove
 
-admin.site.register(Kata)
-admin.site.register(Stance)
+
+@admin.register(Kata)
+class KataAdmin(admin.ModelAdmin):
+    list_display = ['name', 'series', 'hiragana', 'kanji']
+    search_fields = ['name', 'series']
+    list_filter = ['series']
+    ordering = ['pk']
+
+
+@admin.register(Stance)
+class StanceAdmin(admin.ModelAdmin):
+    list_display = ['stance_name', 'hiragana', 'kanji']
+    search_fields = ['stance_name']
+    ordering = ['pk']
+
+
+@admin.register(Technique)
+class TechniqueAdmin(admin.ModelAdmin):
+    list_display = ['technique_name', 'technique_type', 'hiragana', 'kanji']
+    search_fields = ['technique_name', 'technique_type']
+    list_filter = ['technique_type']
+    ordering = ['pk', 'technique_type']
+
+
 admin.site.register(Move)
-admin.site.register(Technique)
+admin.site.register(TechniqueToMove)
