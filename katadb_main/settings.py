c3792a5688deea31c0c00c57055014356a5339f7
cmsato09 <chrismsato@gmail.com>
2023-02-22 19:05:58 -0800

    postgres test settings

--- katadb_main/settings.py
+++ katadb_main/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'katadb_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'karate_test',
+        'USER': 'postgres',
+        'PASSWORD': 'Super',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
