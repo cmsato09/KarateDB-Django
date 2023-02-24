c319f86298b41e39cf0a162ab0ee083d9d782cc3
cmsato09 <chrismsato@gmail.com>
2023-02-23 18:59:32 -0800

    feat: login page

--- katadb_main/urls.py
+++ katadb_main/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('accounts.urls')),
     path('', include('kata_moveset.urls')),
 ]
