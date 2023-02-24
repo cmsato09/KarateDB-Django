fc8ba1dbc44311821af64fc47f65e2c0ce3f3f1f
cmsato09 <chrismsato@gmail.com>
2023-02-23 16:04:51 -0800

    new CustomUser class
    - rolled back migrations for previous users and admins
    - new 'accounts' app with CustomUser model for flexibility in the future
    - made new migrations for new app

--- katadb_main/settings.py
+++ katadb_main/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'django_tables2',
-    'kata_moveset.apps.KataMovesetConfig'
+    'kata_moveset.apps.KataMovesetConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
