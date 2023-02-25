1f48d428a45e48325467a817d0ddbe135cd67b25
cmsato09 <chrismsato@gmail.com>
2023-02-24 17:40:12 -0800

    login-logout links
    - created dashboard page, goes there after login
    - navbar has login, logout links
    - settings.py includes login logout url and redirect url

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+@login_required
+def logged_in_dashboard(request):
+    return render(request, 'accounts/dashboard.html', {'section': 'dashboard'})
--- katadb_main/settings.py
+++ katadb_main/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
