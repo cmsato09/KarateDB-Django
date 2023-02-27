f143a5413075cea43ce52f6cfb2888666c610e43
cmsato09 <chrismsato@gmail.com>
2023-02-27 11:39:14 -0800

    feat: password change form
    - added django.contrib.auth.urls in urlpattern
    - added html forms for password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from .views import logged_in_dashboard
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('django.contrib.auth.urls')),
     path('', logged_in_dashboard, name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,4 @@ from django.shortcuts import render
 @login_required
 def logged_in_dashboard(request):
     return render(request, 'accounts/dashboard.html', {'section': 'dashboard'})
+    
